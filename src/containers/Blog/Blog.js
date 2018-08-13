b2977f44415482565654c985b2000d6fe4cc6409
nitish.thakarar.sa <nitish.thakrar@solutionanalysts.com>
2018-08-13 10:45:51 +0530

    Compoleted 149 Videos of trainning.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,18 +4,44 @@ import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import axios from 'axios';
 
 class Blog extends Component {
-    render () {
+    state = {
+        posts: [],
+        selectedPostId: null
+    }
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts').then(response => {
+            const posts = response.data.slice(0, 4);
+            const updatePosts = posts.map(post => {
+                return { ...post, author: 'Nits' }
+            })
+            this.setState({ posts: updatePosts });
+            // console.log(response);
+        });
+
+    }
+
+    postSelectedHandler = (id) => {
+        this.setState({ selectedPostId: id })
+    }
+    render() {
+        const posts = this.state.posts.map(post => {
+            return <Post
+                key={post.id}
+                title={post.title}
+                author={post.author}
+                clicked={() => { this.postSelectedHandler(post.id) }}
+            />
+        })
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={ this.state.selectedPostId } />
                 </section>
                 <section>
                     <NewPost />
